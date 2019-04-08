1108ba8edb81b4cc2b3f75ecd31feac259d8ee81
sbx_user1051 <sbx_user1051@169.254.156.1>
2019-04-08 10:48:06 +0000

    Mon Apr 08 10:48:06 UTC 2019

--- spec/sum_spec.rb
+++ spec/sum_spec.rb
@@ -4,8 +4,6 @@ require './lib/solutions/SUM/sum.rb'
 describe Sum do
   it 'return a sum of two numbers' do
     num = Num.new
-    expect(num.sum(x,y)).to eq (x,y)
+    expect(num.sum(x,y)).to eq (x+y)
   end
 end
-
-

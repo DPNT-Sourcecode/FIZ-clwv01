4ea17bb5c2472ee3f940abe236ce78070e4f3b18
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 15:34:40 +0000

    Mon Apr 08 15:34:40 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -5,39 +5,37 @@ class FizzBuzz
 
     if ((number % 5 == 0 || number.to_s.include?('5')) && (number % 3 == 0 || number.to_s.include?('3')))
       if (number > 10) && (number.to_s.chars.uniq.length == 1)
-      return "fizz buzz deluxe"
-    else
-      return "fizz buzz"
-    end
+        return "fizz buzz deluxe"
+      else
+        return "fizz buzz"
+      end
 
     elsif (number % 5 == 0 || number.to_s.include?('5'))
       if (number > 10) && (number.to_s.chars.uniq.length == 1)
-      return "buzz deluxe"
-    else
-      return "buzz"
-    end
+        if number % 2 != 0
+          return "buzz fake deluxe"
+        else
+          return "buzz deluxe"
+        end
+      else
+        return "buzz"
+      end
 
-  elsif ((number % 3 == 0) || number.to_s.include?('3'))
-    if (number > 10) && (number.to_s.chars.uniq.length == 1)
-    return "fizz deluxe"
-  else
-    return "fizz"
-  end
+    elsif ((number % 3 == 0) || number.to_s.include?('3'))
+      if (number > 10) && (number.to_s.chars.uniq.length == 1)
+        return "fizz deluxe"
+      else
+        return "fizz"
+      end
     elsif (number > 10) && (number.to_s.chars.uniq.length == 1)
       if number % 2 != 0
         return "fake deluxe"
       else
         return "delux"
       end
-  else
+    else
       return number
     end
-
-
   end
 
 end
-
-
-
-

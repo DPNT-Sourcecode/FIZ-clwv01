80d72939bfa66a017f580ef97872aa2e621c5cee
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 11:49:40 +0000

    Mon Apr 08 11:49:40 UTC 2019

--- test/FIZ/fizz_buzz_spec.rb
+++ test/FIZ/fizz_buzz_spec.rb
@@ -8,14 +8,15 @@ require_solution 'FIZ'
 class ClientTest < Minitest::Test
 
   def test_sum
-    assert_equal fizz, FizzBuzz.new.fizzbuzz(3), 'App should return a fizz'
-    assert_equal buzz, FizzBuzz.new.fizzbuzz(5), 'App should return a buzz'
-    assert_equal fizzbuzz, FizzBuzz.new.fizzbuzz(15), 'App should return a fizzbuzz'
-    assert_equal fizz, FizzBuzz.new.fizzbuzz(6), 'App should return a fizz'
-    assert_equal 7, FizzBuzz.new.fizzbuzz(7), 'App should return a 7'
+    assert_equal 'fizz', FizzBuzz.new.fizzbuzz(3), 'App should return a fizz'
+    assert_equal 'buzz', FizzBuzz.new.fizzbuzz(5), 'App should return a buzz'
+    assert_equal 'fizzbuzz', FizzBuzz.new.fizzbuzz(15), 'App should return a fizzbuzz'
+    assert_equal 'fizz', FizzBuzz.new.fizzbuzz(6), 'App should return a fizz'
+    assert_equal '7', FizzBuzz.new.fizzbuzz(7), 'App should return a 7'
   end
 
 end
+
 
 
 

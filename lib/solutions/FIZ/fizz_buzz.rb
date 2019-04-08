28d7673909170d499fae290256134f61c24dd23e
sbx_user1051 <sbx_user1051@169.254.29.129>
2019-04-08 20:27:12 +0000

    Mon Apr 08 20:27:12 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,5 +2,12 @@
 class FizzBuzz
 
   def fizz_buzz(number)
+    a = []
 
-    
+    a << "fizz" if is_divisible(number, 3)
+    a << "buzz" if is_divisible(number, 5)
+    a << deluxe_type(number) if is_deluxe?(number)
+
+    a.empty? ? number.to_s : prop.join(" ")
+  end
+

5c15bbcc3eae650b44ded286490f1426c0afa125
sbx_user1051 <sbx_user1051@169.254.38.125>
2019-04-08 14:25:05 +0000

    Mon Apr 08 14:25:05 UTC 2019

--- test/FIZ/fizz_buzz_spec.rb
+++ test/FIZ/fizz_buzz_spec.rb
@@ -9,11 +9,14 @@ class ClientTest < Minitest::Test
 
   def test_fiz
     assert_equal 'fizz buzz delux', FizzBuzz.new.fizz_buzz(555), 'App should return a fizz buzz deluxe'
+    assert_equal ' fizz buzz', FizzBuzz.new.fizz_buzz(546), 'App should return a fizz buzz'
     assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'App should return a buzz'
-    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(3), 'App should return a fizz'
-    assert_equal 'deluxe', FizzBuzz.new.fizz_buzz(11), 'App should return a deluxe'
+    assert_equal 'buzz deluxe', FizzBuzz.new.fizz_buzz(55), 'App should return a buzz deluxe'
+    assert_equal 'fizz deluxe', FizzBuzz.new.fizz_buzz(33), 'App should return a buzz fizz deluxe'
+    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(3), 'App should return a buzz fizz'
     assert_equal 7, FizzBuzz.new.fizz_buzz(7), 'App should return a number'
   end
 
 end
+
 

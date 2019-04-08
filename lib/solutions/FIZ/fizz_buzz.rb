eb99adc80cc6f092fd61ba5ff3456bf268596ec5
sbx_user1051 <sbx_user1051@169.254.19.93>
2019-04-08 13:01:32 +0000

    Mon Apr 08 13:01:32 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -4,8 +4,18 @@ class FizzBuzz
   def fizz_buzz(number)
 
     if (number % 5 == 0 || number.to_s.include?('5')) && number % 3 == 0 || (number.to_s.include('3'))
+      return "fizz buzz"
+    elsif (number % 5 == 0 || number.to_s.include?('5'))
+      return "buzz"
+    elsif (number % 3 == 0) || number.to_s.include?('3')
+      return "fizz"
+    else
+      return number
+    end
+
 
   end
 
 end
+
 
--- test/FIZ/fizz_buzz_spec.rb
+++ test/FIZ/fizz_buzz_spec.rb
@@ -8,11 +8,12 @@ require_solution 'FIZ'
 class ClientTest < Minitest::Test
 
   def test_fiz
-    assert_equal 'fizzbuzz', FizzBuzz.new.fizzbuzz(546), 'App should return a fizzbuzz'
+    assert_equal 'fizz buzz', FizzBuzz.new.fizzbuzz(546), 'App should return a fizzbuzz'
     assert_equal 'buzz', FizzBuzz.new.fizzbuzz(5), 'App should return a buzz'
     assert_equal 'fizz', FizzBuzz.new.fizzbuzz(3), 'App should return a fizz'
     assert_equal '7', FizzBuzz.new.fizz_buzz(7), 'App should return a number'
   end
 
 end
+
 

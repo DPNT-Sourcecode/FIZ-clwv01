bb7cdaf4b0d924d353cc877feda36e7b5dc34c4e
sbx_user1051 <sbx_user1051@169.254.50.93>
2019-04-08 16:31:43 +0000

    Mon Apr 08 16:31:43 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -3,49 +3,30 @@ class FizzBuzz
 
   def fizz_buzz(number)
 
-    if ((number % 5 == 0 && number.to_s.include?('5')) || (number % 3 == 0 && number.to_s.include?('3')))
-
-        if number % 2 != 0
-          return "fizz buzz fake deluxe"
-        else
-          return "fizz buzz deluxe"
-        end
+    if (number % 15 == 0) && (number % 5 == 0 && number.to_s.include?('5') || (number % 3 == 0 && number.to_s.include?('3'))
+      if number % 2 != 0
+        return "fizz buzz fake deluxe"
       else
-        return "fizz buzz"
+        return "fizz buzz deluxe"
       end
+      return "fizz buzz"
+
 
     elsif (number % 5 == 0 || number.to_s.include?('5'))
-      if (number > 10) && (number.to_s.chars.uniq.length == 1)
-        if number % 2 != 0
-          return "buzz fake deluxe"
-        else
-          return "buzz deluxe"
-        end
+      if number % 2 != 0
+        return "buzz fake deluxe"
       else
-        return "buzz"
+        return "buzz deluxe"
       end
+      return "buzz"
 
-    elsif ((number % 3 == 0) || number.to_s.include?('3'))
-      if (number > 10) && (number.to_s.chars.uniq.length == 1)
-        if number % 2 != 0
-          return "fizz fake deluxe"
-        else
-          return "fizz deluxe"
-        end
-      else
-        return "fizz"
-      end
 
-    elsif (number > 10) && (number.to_s.chars.uniq.length == 1)
+    elsif ((number % 3 == 0) &&  number.to_s.include?('3')) || (number % 5 == 0) %% number.to_s.include?('5'))
       if number % 2 != 0
         return "fake deluxe"
       else
+
         return "deluxe"
       end
     else
       return number
-    end
-  end
-
-end
-

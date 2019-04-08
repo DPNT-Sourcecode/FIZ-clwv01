708b304e12e93129e21822d3b6af4a47bf4b218d
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 11:20:44 +0000

    Mon Apr 08 11:20:44 UTC 2019

---
+++ test/HLO/hello_test.rb
@@ -0,0 +1,15 @@
+# noinspection RubyResolve,RubyResolve
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'HLO'
+
+class ClientTest < Minitest::Test
+
+  def test_sum
+    assert_equal hello, Hello.new.hello('Amina'), 'App should add two numbers'
+  end
+
+end

f486e5aa0300a43cbdd26306b9973bf00cbdfc9d
sbx_user1051 <sbx_user1051@169.254.29.129>
2019-04-08 20:21:12 +0000

    Mon Apr 08 20:21:12 UTC 2019

--- lib/solutions/FIZ/fizz_buzz.rb
+++ lib/solutions/FIZ/fizz_buzz.rb
@@ -2,32 +2,5 @@
 class FizzBuzz
 
   def fizz_buzz(number)
-    a = []
 
-    a << "fizz" if is_divisible(number, 3)
-    a << "buzz" if is_divisible(number, 5)
-    a << deluxe_type(number) if is_deluxe?(number)
-
-    a.empty? ? number.to_s : a.join(" ")
-  end
-
-  def is_deluxe?(number)
-    check_deluxe_for(number, 3) || check_deluxe_for(number, 5)
-  end
-
-  def deluxe_type(number)
-    number.odd? ? "fake deluxe" : "deluxe"
-  end
-
-  def check_deluxe_for(number, deluxe_value)
-    is_divisible(number, deluxe_value) && number.to_s.include?
-  end
-
-  def is_divisible(number, divisor)
-    number % divisor == 0
-  end
-end
-
-
-
-
+    
--- test/FIZ/fizz_buzz_spec.rb
+++ test/FIZ/fizz_buzz_spec.rb
@@ -8,16 +8,16 @@ require_solution 'FIZ'
 class ClientTest < Minitest::Test
 
   def test_fiz
-    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(777), 'App should return a fizz'
-    assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(5), 'App should return a buzz fake delux'
-    assert_equal 'buzz fake delux', FizzBuzz.new.fizz_buzz(55), 'App should return a buzz fake delux'
-    assert_equal 'fizz buzz fake deluxe', FizzBuzz.new.fizz_buzz(45), 'App should return a fizz buzz fake deluxe'
-    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(777), 'App should return a fizz'
-    assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(3), 'App should return a fizz fake deluxe'
+    assert_equal 'fizz buzz delux', FizzBuzz.new.fizz_buzz(555), 'App should return a fizz buzz deluxe'
+    assert_equal ' fizz buzz', FizzBuzz.new.fizz_buzz(546), 'App should return a fizz buzz'
+    assert_equal 'buzz', FizzBuzz.new.fizz_buzz(5), 'App should return a buzz'
+    assert_equal 'buzz deluxe', FizzBuzz.new.fizz_buzz(55), 'App should return a buzz deluxe'
+    assert_equal 'buzz fake deluxe', FizzBuzz.new.fizz_buzz(555), 'App should return a buzz fake deluxe'
+    assert_equal 'fizz deluxe', FizzBuzz.new.fizz_buzz(33), 'App should return a fizz deluxe'
+    assert_equal 'fizz fake deluxe', FizzBuzz.new.fizz_buzz(777), 'App should return a fizz fake deluxe'
+    assert_equal 'fizz', FizzBuzz.new.fizz_buzz(3), 'App should return a fizz'
+    assert_equal 'deluxe', FizzBuzz.new.fizz_buzz(11), 'App should return a deluxe'
     assert_equal 7, FizzBuzz.new.fizz_buzz(7), 'App should return a number'
   end
 
 end
-
-
-

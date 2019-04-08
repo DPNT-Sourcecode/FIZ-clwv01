277476beb98aefbee2179dc9a90bf37d21895ec2
sbx_user1051 <sbx_user1051@169.254.84.193>
2019-04-08 11:38:13 +0000

    Mon Apr 08 11:38:13 UTC 2019

---
+++ test/FIZ/fizz_buzz_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../test_helper'
+require 'logging'
+
+Logging.logger.root.appenders = Logging.appenders.stdout
+
+require_solution 'SUM'
+
+class ClientTest < Minitest::Test
+
+  def test_sum
+    assert_equal fizz, FizzBuzz.new.fizzbuzz(3), 'App should add two numbers'
+  end
+
+end
